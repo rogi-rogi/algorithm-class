c41ef2609acbec9b529ff10e7960aabbbb62b270
Sung Jun An <144579556+Mutatio02@users.noreply.github.com>
2025-03-20 23:53:50 +0900

    Create 1476.py
    pypy

---
+++ asj777/week-1/1476.py
@@ -0,0 +1,23 @@
+# 25.03.18 C++ -> Python(Data Science) 코드 연습 
+# 날짜 계산 (브루트 포스)
+
+E,S,M = map(int,input().split())
+e,s,m = 1,1,1 # 지구 태양 달 tmp
+year = 1
+
+while(1):
+    if(e==E and s==S and m ==M):
+        print(year)
+        break
+
+    e+=1
+    s+=1
+    m+=1
+    year+=1
+
+    if(e>15):
+        e = 1
+    if(s>28):
+        s = 1
+    if(m>19):
+        m = 1
