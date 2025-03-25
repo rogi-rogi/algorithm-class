bdd105ce0efa88971e03516367c029f15d92dfb5
SungJun An <144579556+Mutatio02@users.noreply.github.com>
2025-03-25 17:34:22 +0900

    Create 10989.py

---
+++ asj777/week-02/10989.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+arr = [0] * 10001  
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    arr[num] += 1  
+
+
+for i in range(1, 10001): 
+    if arr[i] > 0:
+        for _ in range(arr[i]):  
+            print(i)
